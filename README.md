baf3dcf84b3580a5e0c6b046669f69810eebb5e5
lvzhenbo <294221990@qq.com>
2024-05-27 20:59:53 +0800

    添加说明，并测试发布脚本

--- README.md
+++ README.md
@@ -1,18 +1,13 @@
-# Vue 3 + TypeScript + Vite
+# 115+
 
-This template should help get you started developing with Vue 3 and TypeScript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+为115网盘添加一些功能
 
-## Recommended IDE Setup
+## 功能
 
-- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+- 精简侧边栏
+- 删除离线下载项时可默认选择删除源文件
+- 非侵入式获取文件下载直链（非常感谢 [kkHAIKE](https://github.com/kkHAIKE) 提供的 [脚本](https://github.com/kkHAIKE/fake115)，此功能基于这个脚本提供的方法编写）
 
-## Type Support For `.vue` Imports in TS
+## 许可证
 
-TypeScript cannot handle type information for `.vue` imports by default, so we replace the `tsc` CLI with `vue-tsc` for type checking. In editors, we need [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin) to make the TypeScript language service aware of `.vue` types.
-
-If the standalone TypeScript plugin doesn't feel fast enough to you, Volar has also implemented a [Take Over Mode](https://github.com/johnsoncodehk/volar/discussions/471#discussioncomment-1361669) that is more performant. You can enable it by the following steps:
-
-1. Disable the built-in TypeScript Extension
-   1. Run `Extensions: Show Built-in Extensions` from VSCode's command palette
-   2. Find `TypeScript and JavaScript Language Features`, right click and select `Disable (Workspace)`
-2. Reload the VSCode window by running `Developer: Reload Window` from the command palette.
+MIT
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
-import monkey, { cdn, util } from "vite-plugin-monkey";
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import monkey, { cdn, util } from 'vite-plugin-monkey';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +12,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
     AutoImport({
-      imports: ["vue", util.unimportPreset],
+      imports: ['vue', util.unimportPreset],
       eslintrc: {
         enabled: true,
       },
@@ -21,17 +21,17 @@ export default defineConfig({
       resolvers: [NaiveUiResolver()],
     }),
     monkey({
-      entry: "src/main.ts",
+      entry: 'src/main.ts',
       userscript: {
-        icon: "https://115.com/favicon.ico",
-        namespace: "npm/vite-plugin-monkey",
-        match: ["https://115.com/*"],
-        name: "115+",
-        description: "为115网盘添加一些功能",
+        icon: 'https://115.com/favicon.ico',
+        namespace: 'https://github.com/lvzhenbo/115-plus',
+        match: ['https://115.com/*'],
+        name: '115+',
+        description: '为115网盘添加一些功能',
       },
       build: {
         externalGlobals: {
-          vue: cdn.jsdelivr("Vue", "dist/vue.global.prod.js"),
+          vue: cdn.jsdelivr('Vue', 'dist/vue.global.prod.js'),
         },
       },
     }),
