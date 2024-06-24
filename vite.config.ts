1879c795f5351f3779dbb292cd19816a02d07881
lvzhenbo <294221990@qq.com>
2024-06-25 05:33:59 +0800

    将video.js加入远程脚本以解决过长问题

--- vite.config.ts
+++ vite.config.ts
@@ -35,6 +35,7 @@ export default defineConfig({
       build: {
         externalGlobals: {
           vue: cdn.jsdelivr('Vue', 'dist/vue.global.prod.js'),
+          'video.js': cdn.jsdelivr('video.js', 'dist/video.min.js'),
         },
       },
     }),
