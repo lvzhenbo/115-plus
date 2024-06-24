5afe8ee8341df81f27c6e03e7dd489e663e672ce
lvzhenbo <294221990@qq.com>
2024-06-25 05:43:35 +0800

    我好像再次搞错的东西

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
       build: {
         externalGlobals: {
           vue: cdn.jsdelivr('Vue', 'dist/vue.global.prod.js'),
-          videojs: cdn.jsdelivr('video.js', 'dist/video.min.js'),
+          'video.js': cdn.jsdelivr('videojs', 'dist/video.min.js'),
         },
       },
     }),
