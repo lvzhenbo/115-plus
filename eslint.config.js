d7c9b9c3d984d1764bee0a626d9dc98fe11367e1
lvzhenbo <294221990@qq.com>
2025-02-10 04:18:10 +0800

    更新 ESLint 配置，添加新规则和全局变量；更新依赖项以提升性能和兼容性

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,23 @@
-import { defineFlatConfig } from 'eslint-define-config';
 import js from '@eslint/js';
 import pluginVue from 'eslint-plugin-vue';
-import vueTsEslintConfig from '@vue/eslint-config-typescript';
+import {
+  defineConfigWithVueTs,
+  vueTsConfigs,
+  configureVueProject,
+} from '@vue/eslint-config-typescript';
 import prettierConfig from '@vue/eslint-config-prettier';
 import parserVue from 'vue-eslint-parser';
 import AutoImport from './.eslintrc-auto-import.json' with { type: 'json' };
 
-export default defineFlatConfig([
-  ...pluginVue.configs['flat/recommended'],
+configureVueProject({
+  tsSyntaxInTemplates: true,
+  scriptLangs: ['ts', 'tsx'],
+});
+
+export default defineConfigWithVueTs(
+  pluginVue.configs['flat/recommended'],
   js.configs.recommended,
-  ...vueTsEslintConfig({
-    supportedScriptLangs: {
-      ts: true,
-      tsx: true,
-    },
-  }),
+  vueTsConfigs.recommended,
   {
     name: 'app/vue-files',
     files: ['**/*.vue'],
@@ -46,4 +49,4 @@ export default defineFlatConfig([
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
   prettierConfig,
-]);
+);
